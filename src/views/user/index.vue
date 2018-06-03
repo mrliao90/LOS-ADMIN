aab04a5bbf7cdedf5a969e754eb757e0682a47c9
junrong.liao <429170250@qq.com>
2018-06-03 13:36:21 +0800

    idwork

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="app-container">
+    <p>1111111111111</p>
     <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
       <el-table-column align="center" label='ID' width="95">
         <template slot-scope="scope">
-          {{scope.$index}}
+          {{scope.$index+1}}
         </template>
       </el-table-column>
       <el-table-column label="登陆账号">
         <template slot-scope="scope">
-          {{scope.row.title}}
+          {{scope.row.username}}
         </template>
       </el-table-column>
-      <el-table-column label="用户名" width="110" align="center">
+      <el-table-column label="昵称"  align="center">
         <template slot-scope="scope">
-          <span>{{scope.row.author}}</span>
+          <span>{{scope.row.realname}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="密码" width="110" align="center">
+      <el-table-column label="Email" align="center">
         <template slot-scope="scope">
-          {{scope.row.pageviews}}
+          {{scope.row.email}}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="状态" width="110" align="center">
+      <el-table-column class-name="status-col" label="状态"  align="center">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="创建时间" width="200">
+      <el-table-column align="center" prop="createTime" label="创建时间" >
         <template slot-scope="scope">
           <i class="el-icon-time"></i>
-          <span>{{scope.row.display_time}}</span>
+          <span>{{scope.row.createTime}}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -61,9 +62,11 @@ export default {
   },
   methods: {
     fetchData() {
+      debugger
       this.listLoading = true
       getList(this.listQuery).then(response => {
-        this.list = response.data.items
+        debugger
+        this.list = response.data
         this.listLoading = false
       })
     }
